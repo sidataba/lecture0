9cf8c7b6c6f5d4aebace935778c18a2cc0939ecd
sidataba <hieuhip4444@gmail.com>
2025-03-02 20:37:04 +0700

    direct to main

--- loaddataset.py
+++ loaddataset.py
@@ -31,3 +31,42 @@ class NeuralNetwork(torch.nn.Module):
     
 model = NeuralNetwork().to(device)
 print(model)
+
+x = torch.rand(1, 28, 28, device=device)
+logits = model(x)
+pred_probab = nn.Softmax(dim=1)(logits)
+y_pred = pred_probab.argmax(1)
+print(f"Predicted class: {y_pred}")
+
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+print(f"Before ReLU: {hidden1}")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
+
+seq_modules = nn.Sequential(
+    flatten,
+    layer1,
+    nn.ReLU(),
+    nn.Linear(20, 10)
+)
+input_image = torch.rand(3,28,28)
+logits = seq_modules(input_image)
+
+softmax = nn.Softmax(dim=1)
+pred_probab = softmax(logits)
+
+print(f"Model structure: {model}\n\n")
+
+for name, param in model.named_parameters():
+    print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
+
